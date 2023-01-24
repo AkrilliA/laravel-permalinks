532986d03326f434817ba4417b7c6dcb832617b8
Alexander Gaal <alexander.gaal@gipfel.dev>
2023-01-24 12:09:35 +0100

    fix wrong column

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use AkrilliA\LaravelPermalinks\Models\Permalink;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{permalink}', function ($permalink) {
-    $permalink = Permalink::query()->where('permalink', $permalink)->firstOrFail();
+    $permalink = Permalink::query()->where('url', $permalink)->firstOrFail();
 
     return redirect($permalink->url);
 })->name('permalink');
